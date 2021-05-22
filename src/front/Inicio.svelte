a96e9c286dec0abdcde8de9490dee7b2ad09e050
enrike7lg <enri7luk@hotmail.com>
2021-05-22 21:55:57 +0200

    api

--- src/front/Inicio.svelte
+++ src/front/Inicio.svelte
@@ -6,17 +6,11 @@ import { Jumbotron, Button } from "sveltestrap";
 	<Jumbotron>
         <h1 class="display-3">¡Bienvenid@!</h1>
         <p class="lead">
-            <strong>El contenido de esta web ha sido desarrollado por:</strong>
+            <strong>El contenido de esta API ha sido desarrollado por:</strong>
         </p>
         <hr class="my-2" />
         <p>
             <a href="https://github.com/enrike7lg">Enrique Luque García</a>
-        </p>
-        <p>
-            <a href="https://github.com/framacvaz ">Francisco Javier Macero Vázquez</a>
-        </p>
-        <p>
-            <a href="https://github.com/juaroltor ">Juan Ignacio Roldán Torres </a>
         </p>
         <p>
             Para más información, haz click aquí:
