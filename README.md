ec4ccb315d459f2f96dc6c10393ae6f3d7004f99
enrike7lg <enri7luk@hotmail.com>
2021-05-24 18:03:44 +0200

    end point

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # API
+- [API](http://sos2021-children-out-school.herokuapp.com/)
+- [API END POINT](http://sos2021-children-out-school.herokuapp.com/api/v2/children-out-school)
--- src/back/schoolAPI/v2/index.js
+++ src/back/schoolAPI/v2/index.js
@@ -136,7 +136,7 @@ module.exports.init = (app) => {
 ];
 
 //Delete the database
-db.remove({}, {multi: true});
+//db.remove({}, {multi: true});
 
 //GET loadInitialData children-out-school
 app.get(BASE_CHILDREN_OUT_SCHOOL_API_PATH  + "/loadInitialData", (req, res) => {
